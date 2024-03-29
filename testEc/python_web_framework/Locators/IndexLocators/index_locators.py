9d4e673d4c9db96680bf96d2253a99c0893d3f39
lili.li <liyunduanyue@163.com>
2024-03-29 11:26:39 +0800

    f

--- testEc/python_web_framework/Locators/IndexLocators/index_locators.py
+++
@@ -1,7 +0,0 @@
-from selenium.webdriver.common.by import By
-
-
-class IndexLocator:
-    logout_loc = (By.XPATH, '//a[text()="退出"]')
-
-
--- testEc/python_web_framework/TestDatas/GobalDatas/gobal_datas.py
+++
@@ -1,3 +0,0 @@
-web_login_url = 'http://192.168.200.148:8085/login'
-
-web_user_url = 'http://192.168.200.148:8085/system/user'
